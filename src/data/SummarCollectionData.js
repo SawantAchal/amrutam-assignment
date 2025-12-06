a08340ae73118a62a40b1aeda32cb4ed1940e1cf
SawantAchal <achals1718@gmail.com>
2025-12-07 01:50:40 +0530

    dummy datat for summar collection

---
+++ src/data/SummarCollectionData.js
@@ -0,0 +1,35 @@
+export const summarCollectionData = [
+  {
+    id: 1,
+    title: "Amrutam Kuntal care Hair Spa | Do–it yourself Hair Treatment",
+    price: 649.0,
+    size: "200 ml",
+    oldPrice: 649.0,
+    rating: 5,
+    reviewsCount: 52,
+    image: '/summarCollection1.png',
+    tag: "Summer Collection"
+  },
+  {
+    id: 2,
+    title: "Amrutam Kuntal Care Herbal Shampoo | Healthy, Natural and Dynamic Hair",
+    price: 649.0,
+    size: "200 ml",
+    oldPrice: 649.0,
+    rating: 5,
+    reviewsCount: 52,
+    image:  '/summarCollecton2.png',
+    tag: "Summer Collection"
+  },
+  {
+    id: 3,
+    title: "Amrutam Nari Sondarya Malt | Complete Care For Women's Health And Beauty",
+    price: 649.0,
+    size: "200 ml",
+    oldPrice: 649.0,
+    rating: 5,
+    reviewsCount: 52,
+    image:'/summarCollection3.png',
+    tag: "Summer Collection"
+  }
+];
