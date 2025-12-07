e0134fefca3784bb52211932ec43c97038898770
SawantAchal <achals1718@gmail.com>
2025-12-07 20:06:35 +0530

    Date for experts list

---
+++ src/data/MeetOurExpertData.js
@@ -0,0 +1,32 @@
+export const meetOurExpertsData = [
+    {
+        id: 1,
+        name: "Dr. Vaishali Sharma",
+        profileImage: "/MeetProfileImage.png",
+        rating: 4.5,
+        qualification: "Ayurveda Practitioner (BAMS, MD)",
+        experienceYears: 25,
+        speciality: "Skin Specialist",
+        ctaText: "Book a session"
+    },
+    {
+    id: 2,
+    name: "Dr. Vaishali Sharma",
+    profileImage: "/MeetProfileImage.png",
+    rating: 4.5,
+    qualification: "Ayurveda Practitioner (BAMS, MD)",
+    experienceYears: 25,
+    speciality: "Skin Specialist",
+    ctaText: "Book a session"
+    },
+    {
+        id: 3,
+        name: "Dr. Vaishali Sharma",
+        profileImage: "/MeetProfileImage.png",
+        rating: 4.5,
+        qualification: "Ayurveda Practitioner (BAMS, MD)",
+        experienceYears: 25,
+        speciality: "Skin Specialist",
+        ctaText: "Book a session"
+    }
+];
