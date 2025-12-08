bd24699b1bb76c80c858f14142d7a56ede8d5864
SawantAchal <achals1718@gmail.com>
2025-12-08 22:36:01 +0530

    add route for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
+import Home from './components/Home'
+import ProductDetailed from './components/ProductDetailed'
+import IngrediateDetail from './components/IngrediateDetail'
 
 function App() {
-
-
   return (
     <>
-      <HomePage/>
+      <Routes>
+        <Route path='/' element={<HomePage/>}>
+          <Route index element={<Home/>}/>
+          <Route path="/product/:id" element={<ProductDetailed/>} />
+          <Route path="/ingredient" element={<IngrediateDetail />} />
+        </Route>
+      </Routes>
     </>
   )
 }
