80843ed027e33e641932f8e2ee939344f65ffaf3
SawantAchal <achals1718@gmail.com>
2025-12-10 16:53:06 +0530

    add  citrak data

---
+++ src/data/IngredientData.js
@@ -0,0 +1,187 @@
+export const ingredientData = [
+  {
+  id: 1,
+  name: "Gokhru",
+  slug: "gokhru",
+  image: "/gokhruIng.png",
+  shortDescription:
+    "Also known as Tribulus, it’s used to prevent infections and manage diabetes.",
+  longDescription:
+    "Also known as Tribulus, it's used to prevent infections and manage diabetes. It's known for its potential to boost testosterone levels and improve reproductive health.",
+  productsTitle: "Products with “Gokhru” as primary ingredient",
+  products: [
+    {
+      id: 101,
+      title:
+        "Amrutam Kuntal Care Herbal Shampoo | Healthy, Natural and Dynamic Hair",
+      price: 649,
+      size: "200 ml",
+      image: "/summarCollecton2.png",
+    },
+    {
+      id: 102,
+      title:
+        "Amrutam Kuntal Care Herbal Shampoo | Healthy, Natural and Dynamic Hair",
+      price: 649,
+      size: "200 ml",
+      image: "/summarCollecton2.png",
+    },
+    {
+      id: 103,
+      title:
+        "Amrutam Kuntal Care Herbal Shampoo | Healthy, Natural and Dynamic Hair",
+      price: 649,
+      size: "200 ml",
+      image: "/summarCollecton2.png",
+    },
+    {
+      id: 104,
+      title:
+        "Amrutam Kuntal Care Herbal Shampoo | Healthy, Natural and Dynamic Hair",
+      price: 649,
+      size: "200 ml",
+      image: "/summarCollecton2.png",
+    },
+  ],
+},
+{
+  id: 2,
+  name: "Citrak",
+  botanical_name: "Plumbago zeylanica",
+  sanskrit_name: "चित्रक",
+  image:"/citrak.png",
+  description: "Chitrak, also known as Ceylon Leadwort or Doctorbush, is an important Ayurvedic herb that supports digestive fire (Agni), metabolism, and circulation. It is known for balancing Vata and Kapha while increasing Pitta.",
+  why_citrak: [
+    "Chitrak is valued because it helps lower blood sugar, boosts digestion, and reduces anxiety.",
+    "It also protects the skin and speeds up wound healing with its antioxidant and antimicrobial properties.",
+    "It is most used in Ayurvedic medicines for indigestion."
+  ],
+
+  prakriti_impact: {
+    vata: "Balanced",
+    kapha: "Balanced",
+    pitta: "Imbalanced"
+  },
+
+  benefits: [
+    "Helps promote gastric juices (Deepana and Pitta Sthapana action)",
+    "Improves digestion and helps in lowering stress",
+    "Supports digestion and acidity relief",
+    "Reduces inflammation (Shothahara)",
+    "Promotes bowel movement (Vibandhaharaa)",
+    "Assists in removing mucus from respiratory pathways",
+    "Helps maintain lipid levels and reduce cholesterol",
+    "Supports weight balance and metabolism",
+  ],
+
+
+  ayurvedic_properties: [
+    {
+      "image" : "",
+      "key": "rasa",
+      "name": "Rasa",
+      "value": "Katu (Pungent)",
+      "description": "Stimulates salivation and digestion"
+    },
+    {
+      "image" : "",
+      "key": "veerya",
+      "name": "Veerya",
+      "value": "Ushna (Hot)",
+      "description": "Increases heat and energy in the body"
+    },
+    {
+      "image" : "",
+      "key": "guna",
+      "name": "Guna",
+      "value": "Laghu (Light), Ruksha (Dry), Tikshna (Sharp)",
+      "description": "Easy to digest, reduces heaviness"
+    },
+    {
+      "image" : "",
+      "key": "vipaka",
+      "name": "Vipaka",
+      "value": "Katu (Pungent)",
+      "description": "Drying and stimulating even after digestion"
+    }
+  ]
+,
+
+
+  important_formulations: [
+    {
+      icon:"",
+      name:"Chitrakadi Vati",
+    },
+        {
+      icon:"",
+      name:"Chitrak Haritaki",
+    },
+        {
+      icon:"",
+      name:"Kalyanagulam",
+    },
+        {
+      icon:"",
+      name:"Chitrakadi Churna"
+    }
+  ],
+
+  therapeutic_uses: [
+    {
+      icon : "",
+      name :"Agnimandya" ,
+    },
+        {
+      icon : "",
+      name : "Grahani Rog",
+    },
+        {
+      icon : "",
+      name : "Arsha",
+    }
+    ,
+        {
+      icon : "",
+      name : "Udara Shula",
+    }
+    ,
+        {
+      icon : "",
+      name : "Gudasotha ",
+    }
+  ],
+
+  plant_part_used: [
+  {
+    name: "Root",
+    icon: "/icons/root.svg",
+    purpose: "Digestion, skin conditions, manage blood sugar level."
+  },
+  {
+    name: "Root bark",
+    icon: "/icons/root-bark.svg",
+    purpose: "Manage obesity, metabolism and assist in weight loss."
+  },
+  {
+    name: "Leaves",
+    icon: "/icons/leaves.svg",
+    purpose: "Used externally for skin conditions and wounds."
+  }
+  ],
+
+  best_combined_with: [
+    'Pipplai', 'Haritakai', 'Guggulu', 'Punarnava', 'Amla', 'Giloy'
+  ],
+
+  geographical_locations: [
+    "It is native to tropical and subtropical regions worldwide, including India and Srilanka."
+  ]
+}
+
+]
+
+
+
+
+
